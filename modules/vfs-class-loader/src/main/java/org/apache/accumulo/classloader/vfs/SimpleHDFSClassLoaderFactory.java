56abcb4860fb2967f7e4f6923c22558b7fe4fac3
Kevin Rathbun <kevinrr888@gmail.com>
2025-09-23 16:09:06 -0400

    New SimpleHDFSClassLoaderFactory class
    Creates SimpleHDFSClassLoaderFactory which will replace the use of the (buggy) VFSClassLoader with URLClassLoader while still supporting a ClassLoader per context:
    
    * SimpleHDFSClassLoaderFactory will be the replacement for the ReloadingVFSContextClassLoaderFactory and uses URLClassLoader instead of VFSClassLoader
    * Created SimpleHDFSClassLoaderFactoryTest
    * Bumped accumulo-classloaders from accumulo 2.1.3 to 2.1.4
    * Minor javadoc fix for ReloadingVFSContextClassLoaderFactory
    
    Initial commit for #27

---
+++ modules/vfs-class-loader/src/main/java/org/apache/accumulo/classloader/vfs/SimpleHDFSClassLoaderFactory.java
@@ -0,0 +1,140 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.accumulo.classloader.vfs;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.accumulo.core.conf.AccumuloConfiguration;
+import org.apache.accumulo.core.spi.common.ContextClassLoaderFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.gson.Gson;
+
+public class SimpleHDFSClassLoaderFactory implements ContextClassLoaderFactory {
+  private static final Logger LOG = LoggerFactory.getLogger(SimpleHDFSClassLoaderFactory.class);
+  // TODO KEVIN RATHBUN see URLContextClassLoaderFactory from main
+  // Cache the class loaders for re-use
+  // WeakReferences are used so that the class loaders can be cleaned up when no longer needed
+  // Classes that are loaded contain a reference to the class loader used to load them
+  // so the class loader will be garbage collected when no more classes are loaded that reference it
+  private final Cache<Path,URLClassLoader> classLoaders =
+      CacheBuilder.newBuilder().weakValues().build();
+  // TODO KEVIN RATHBUN need to get from a new configurable prop
+  private final Path userDefDir = Path.of(System.getProperty("user.dir"), "target", "contexts");
+  private static final String MANIFEST_FILE_NAME = "manifest.json";
+
+  @VisibleForTesting
+  public static class Context {
+    private String contextName;
+    private List<JarInfo> jars;
+
+    public String getContextName() {
+      return contextName;
+    }
+
+    public void setContextName(String contextName) {
+      this.contextName = contextName;
+    }
+
+    public List<JarInfo> getJars() {
+      return jars;
+    }
+
+    public void setJars(List<JarInfo> jars) {
+      this.jars = jars;
+    }
+  }
+
+  @VisibleForTesting
+  public static class JarInfo {
+    private String jarName;
+    private String checksum;
+
+    public String getJarName() {
+      return jarName;
+    }
+
+    public void setJarName(String jarName) {
+      this.jarName = jarName;
+    }
+
+    public String getChecksum() {
+      return checksum;
+    }
+
+    public void setChecksum(String checksum) {
+      this.checksum = checksum;
+    }
+  }
+
+  public SimpleHDFSClassLoaderFactory(AccumuloConfiguration conf) {
+    try {
+      initializeContexts(conf);
+    } catch (IOException e) {
+      throw new IllegalStateException(this.getClass().getSimpleName() + " could not be created.",
+          e);
+    }
+  }
+
+  private void initializeContexts(AccumuloConfiguration conf) throws IOException {
+    if (classLoaders.size() != 0) {
+      LOG.debug("Contexts already initialized, skipping...");
+      return;
+    }
+    final Set<Path> allContextPaths;
+    try (var childPathsStream = Files.list(userDefDir)) {
+      allContextPaths = childPathsStream.filter(Files::isDirectory).collect(Collectors.toSet());
+    }
+    for (var contextPath : allContextPaths) {
+      Path manifestFile = contextPath.resolve(MANIFEST_FILE_NAME);
+      createClassLoader(manifestFile);
+    }
+  }
+
+  private void createClassLoader(Path manifestFile) throws IOException {
+    Context jsonData = new Gson().fromJson(Files.newBufferedReader(manifestFile), Context.class);
+    URL[] urls = new URL[jsonData.getJars().size()];
+    int i = 0;
+    for (var jar : jsonData.getJars()) {
+      urls[i++] = manifestFile.getParent().resolve(jar.getJarName()).toUri().toURL();
+    }
+    classLoaders.put(manifestFile, new URLClassLoader(urls, ClassLoader.getSystemClassLoader()));
+  }
+
+  @Override
+  public ClassLoader getClassLoader(String contextName) throws ContextClassLoaderException {
+    var expectedPath = userDefDir.resolve(contextName).resolve(MANIFEST_FILE_NAME);
+    var classLoader = classLoaders.getIfPresent(expectedPath);
+    if (classLoader == null) {
+      throw new ContextClassLoaderException(contextName);
+    }
+    return classLoader;
+  }
+}
---
+++ modules/vfs-class-loader/src/test/java/org/apache/accumulo/classloader/vfs/context/SimpleHDFSClassLoaderFactoryTest.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.accumulo.classloader.vfs.context;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.FileWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.accumulo.classloader.vfs.SimpleHDFSClassLoaderFactory;
+import org.apache.accumulo.classloader.vfs.SimpleHDFSClassLoaderFactory.Context;
+import org.apache.accumulo.classloader.vfs.SimpleHDFSClassLoaderFactory.JarInfo;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+
+public class SimpleHDFSClassLoaderFactoryTest {
+  // TODO KEVIN RATHBUN SimpleHDFSClassLoaderFactory and this test need to be moved to their own
+  // package (currently in vfs-class-loader)
+  private static final Logger LOG = LoggerFactory.getLogger(SimpleHDFSClassLoaderFactoryTest.class);
+  private static final String context1 = "context1";
+  private static final Path contextsDir =
+      Path.of(System.getProperty("user.dir"), "target", "contexts");
+  private static final Path context1Dir = contextsDir.resolve(context1);
+  private static final Path context1Manifest = context1Dir.resolve("manifest.json");
+
+  @BeforeAll
+  public static void setup() throws Exception {
+    assertTrue(contextsDir.toFile().mkdir());
+    assertTrue(context1Dir.toFile().mkdir());
+
+    JarInfo jarInfo1 = new JarInfo();
+    jarInfo1.setJarName("HelloWorld.jar");
+    jarInfo1.setChecksum("123abc");
+
+    Context context = new Context();
+    context.setContextName(context1);
+    context.setJars(List.of(jarInfo1));
+
+    Gson gson = new Gson().newBuilder().setPrettyPrinting().create();
+    try (FileWriter writer = new FileWriter(context1Manifest.toFile(), StandardCharsets.UTF_8)) {
+      gson.toJson(context, writer);
+    }
+  }
+
+  @AfterAll
+  public static void teardown() throws Exception {
+    FileUtils.deleteDirectory(contextsDir.toFile());
+  }
+
+  @Test
+  public void test() throws Exception {
+    FileUtils.copyURLToFile(this.getClass().getResource("/HelloWorld.jar"),
+        context1Dir.resolve("HelloWorld.jar").toFile());
+
+    var classLoaderFactory = new SimpleHDFSClassLoaderFactory(null);
+    var classLoader = classLoaderFactory.getClassLoader(context1);
+    var clazz = classLoader.loadClass("test.HelloWorld");
+    var methods = clazz.getMethods();
+    assertEquals(1,
+        Arrays.stream(methods).filter(method -> method.getName().equals("helloWorld")).count());
+  }
+}
