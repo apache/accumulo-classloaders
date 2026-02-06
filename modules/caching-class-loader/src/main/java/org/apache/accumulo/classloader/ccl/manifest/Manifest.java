818575c20089c3a8552defdead6b5c05456c7d6e
Christopher Tubbs <ctubbsii@apache.org>
2026-02-06 16:54:22 -0500

    Create init utility
    * Move CLI commands into separate package
    * Minor refactor to allow LocalStore to be initialized and to store
      without creating a classloader to avoid utility from leaving behind
      unnecessary working directories
    * Ensure monitor threads are daemon threads (observed that they held up
      process exit before refactoring LocalStore to be used without creating
      a classloader; now unneeded, but still a good change)

--- modules/caching-class-loader/src/main/java/org/apache/accumulo/classloader/ccl/manifest/Manifest.java
+++ modules/caching-class-loader/src/main/java/org/apache/accumulo/classloader/ccl/manifest/Manifest.java
@@ -28,22 +28,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-import org.apache.accumulo.start.spi.KeywordExecutable;
 import org.apache.commons.codec.digest.DigestUtils;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
-import com.beust.jcommander.ParameterException;
-import com.google.auto.service.AutoService;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Suppliers;
 import com.google.gson.Gson;
@@ -51,59 +43,7 @@ import com.google.gson.GsonBuilder;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
-@AutoService(KeywordExecutable.class)
-public class Manifest implements KeywordExecutable {
-
-  static class Opts {
-    @Parameter(names = {"-i", "--interval"}, required = true,
-        description = "monitor interval (in seconds)", arity = 1, order = 1)
-    int monitorInterval;
-
-    @Parameter(names = {"-a", "--algorithm"}, required = false,
-        description = "checksum algorithm to use (default: " + SHA_512 + ")", arity = 1, order = 2)
-    String algorithm = SHA_512;
-
-    @Parameter(required = true, description = "classpath element URL (<url>[ <url>...])",
-        arity = -1, order = 3)
-    public List<String> files = new ArrayList<>();
-
-    @Parameter(names = {"-h", "-?", "--help", "-help"}, help = true)
-    public boolean help = false;
-
-    void parseArgs(Consumer<JCommander> jcConsumer, String programName, String[] args,
-        Object... others) {
-      JCommander commander = new JCommander();
-      jcConsumer.accept(commander);
-      commander.addObject(this);
-      for (Object other : others) {
-        commander.addObject(other);
-      }
-      commander.setProgramName(programName);
-      try {
-        commander.parse(args);
-      } catch (ParameterException ex) {
-        commander.usage();
-        exitWithError(ex.getMessage(), 1);
-      }
-      if (help) {
-        commander.usage();
-        exit(0);
-      }
-    }
-
-    void parseArgs(String programName, String[] args, Object... others) {
-      parseArgs(jCommander -> {}, programName, args, others);
-    }
-
-    void exit(int status) {
-      System.exit(status);
-    }
-
-    void exitWithError(String message, int status) {
-      System.err.println(message);
-      exit(status);
-    }
-  }
+public class Manifest {
 
   // pretty-print uses Unix newline
   private static final Gson GSON =
@@ -200,25 +140,4 @@ public class Manifest implements KeywordExecutable {
     return GSON.toJson(this).stripTrailing() + "\n";
   }
 
-  @Override
-  public String keyword() {
-    return "create-classloader-manifest";
-  }
-
-  @Override
-  public String description() {
-    return "Creates and prints a class loader context manifest for the CachingClassLoaderFactory";
-  }
-
-  @Override
-  public void execute(String[] args) throws Exception {
-    Opts opts = new Opts();
-    opts.parseArgs(Manifest.class.getName(), args);
-    URL[] urls = new URL[opts.files.size()];
-    int count = 0;
-    for (String f : opts.files) {
-      urls[count++] = new URL(f);
-    }
-    System.out.print(create(opts.monitorInterval, opts.algorithm, urls).toJson());
-  }
 }
