d20da30a091b63cd0142ad16299ec660336729a6
Christopher Tubbs <ctubbsii@apache.org>
2026-02-06 14:38:06 -0500

    Rebrand to CachingClassLoaderFactory (#68)
    * Use shorter name "Caching" (dropping "Local" and "Context") for improved branding
    * Shorten "ContextDefinition" to "Manifest"
    * Improve javadoc and README
    * Reduce class visiblity (reduce the number that are public) and
      consolidate into fewer packages (also helps with Logger names, since
      the parent package will be "cargo" for the Logger instances)
    * Make optional grace period property re-configurable on a running
      system

---
+++ modules/caching-class-loader/src/main/java/org/apache/accumulo/classloader/ccl/DeduplicationCache.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.accumulo.classloader.ccl;
+
+import static java.util.Objects.requireNonNull;
+
+import java.time.Duration;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.RemovalListener;
+import com.github.benmanes.caffeine.cache.Scheduler;
+
+/**
+ * A simple de-duplication cache of weakly referenced values that retains a strong reference for a
+ * minimum amount of time specified, to prevent garbage collection too frequently for objects that
+ * may be used again.
+ */
+class DeduplicationCache<KEY,PARAMS,VALUE> {
+
+  private static final Logger LOG = LoggerFactory.getLogger(DeduplicationCache.class);
+
+  private final Cache<KEY,VALUE> canonicalWeakValuesCache;
+  private final Cache<KEY,VALUE> expireAfterAccessStrongRefs;
+  private final BiFunction<KEY,PARAMS,VALUE> loaderFunction;
+
+  private final RemovalListener<KEY,VALUE> logListener = (key, value, cause) -> LOG
+      .trace("Entry removed due to {}. K = {}, V = {}", cause, key, value);
+
+  public DeduplicationCache(final BiFunction<KEY,PARAMS,VALUE> loaderFunction,
+      final Duration minLifetime, final RemovalListener<KEY,VALUE> listener) {
+    this.loaderFunction = requireNonNull(loaderFunction);
+    RemovalListener<KEY,VALUE> actualListener =
+        listener == null ? logListener : (key, value, cause) -> {
+          logListener.onRemoval(key, value, cause);
+          listener.onRemoval(key, value, cause);
+        };
+    this.canonicalWeakValuesCache =
+        Caffeine.newBuilder().weakValues().evictionListener(actualListener).build();
+    this.expireAfterAccessStrongRefs =
+        Caffeine.newBuilder().expireAfterAccess(requireNonNull(minLifetime))
+            .evictionListener(actualListener).scheduler(Scheduler.systemScheduler()).build();
+  }
+
+  public VALUE computeIfAbsent(final KEY key, final Supplier<PARAMS> params) {
+    var cl = canonicalWeakValuesCache.get(key, k -> loaderFunction.apply(k, params.get()));
+    expireAfterAccessStrongRefs.put(key, cl);
+    return cl;
+  }
+
+  public boolean anyMatch(final Predicate<KEY> keyPredicate) {
+    canonicalWeakValuesCache.cleanUp();
+    return canonicalWeakValuesCache.asMap().keySet().stream().anyMatch(keyPredicate);
+  }
+
+  public void forEach(final BiConsumer<KEY,VALUE> consumer) {
+    canonicalWeakValuesCache.cleanUp();
+    canonicalWeakValuesCache.asMap().forEach((k, v) -> {
+      if (v != null) {
+        consumer.accept(k, v);
+      }
+    });
+  }
+
+}
---
+++ modules/caching-class-loader/src/main/java/org/apache/accumulo/classloader/ccl/manifest/Manifest.java
@@ -0,0 +1,224 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.accumulo.classloader.ccl.manifest;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Objects.hash;
+import static java.util.Objects.requireNonNull;
+
+import java.io.BufferedInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import org.apache.accumulo.start.spi.KeywordExecutable;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.auto.service.AutoService;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Suppliers;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+@AutoService(KeywordExecutable.class)
+public class Manifest implements KeywordExecutable {
+
+  static class Opts {
+    @Parameter(names = {"-i", "--interval"}, required = true,
+        description = "monitor interval (in seconds)", arity = 1, order = 1)
+    int monitorInterval;
+
+    @Parameter(names = {"-a", "--algorithm"}, required = false,
+        description = "checksum algorithm to use (default: " + SHA_512 + ")", arity = 1, order = 2)
+    String algorithm = SHA_512;
+
+    @Parameter(required = true, description = "classpath element URL (<url>[ <url>...])",
+        arity = -1, order = 3)
+    public List<String> files = new ArrayList<>();
+
+    @Parameter(names = {"-h", "-?", "--help", "-help"}, help = true)
+    public boolean help = false;
+
+    void parseArgs(Consumer<JCommander> jcConsumer, String programName, String[] args,
+        Object... others) {
+      JCommander commander = new JCommander();
+      jcConsumer.accept(commander);
+      commander.addObject(this);
+      for (Object other : others) {
+        commander.addObject(other);
+      }
+      commander.setProgramName(programName);
+      try {
+        commander.parse(args);
+      } catch (ParameterException ex) {
+        commander.usage();
+        exitWithError(ex.getMessage(), 1);
+      }
+      if (help) {
+        commander.usage();
+        exit(0);
+      }
+    }
+
+    void parseArgs(String programName, String[] args, Object... others) {
+      parseArgs(jCommander -> {}, programName, args, others);
+    }
+
+    void exit(int status) {
+      System.exit(status);
+    }
+
+    void exitWithError(String message, int status) {
+      System.err.println(message);
+      exit(status);
+    }
+  }
+
+  // pretty-print uses Unix newline
+  private static final Gson GSON =
+      new GsonBuilder().disableJdkUnsafe().setPrettyPrinting().create();
+
+  @SuppressFBWarnings(value = "URLCONNECTION_SSRF_FD",
+      justification = "user-supplied URL is the intended functionality")
+  public static Manifest create(int monitorIntervalSecs, String algorithm, URL... sources)
+      throws IOException {
+    // use a LinkedHashSet to preserve the order of the resources
+    LinkedHashSet<Resource> resources = new LinkedHashSet<>();
+    for (URL u : sources) {
+      try (InputStream is = new BufferedInputStream(u.openStream())) {
+        String checksum = new DigestUtils(algorithm).digestAsHex(is);
+        resources.add(new Resource(u, algorithm, checksum));
+      }
+    }
+    return new Manifest(monitorIntervalSecs, resources);
+  }
+
+  @SuppressFBWarnings(value = "URLCONNECTION_SSRF_FD",
+      justification = "user-supplied URL is the intended functionality")
+  public static Manifest download(final URL url) throws IOException {
+    try (InputStream is = url.openStream()) {
+      var manifest = GSON.fromJson(new InputStreamReader(is, UTF_8), Manifest.class);
+      if (manifest == null) {
+        throw new EOFException("InputStream does not contain a valid manifest at " + url);
+      }
+      return manifest;
+    }
+  }
+
+  private static final String SHA_512 = "SHA-512";
+
+  // transient fields that don't go in the json
+  private final transient Supplier<String> checksum =
+      Suppliers.memoize(() -> new DigestUtils(getChecksumAlgorithm()).digestAsHex(toJson()));
+
+  // serialized fields for json
+  // use a LinkedHashSet to preserve the order specified in the file
+  private int monitorIntervalSeconds;
+  private LinkedHashSet<Resource> resources;
+
+  public Manifest() {}
+
+  public Manifest(int monitorIntervalSeconds, LinkedHashSet<Resource> resources) {
+    Preconditions.checkArgument(monitorIntervalSeconds > 0,
+        "monitor interval must be greater than zero");
+    this.monitorIntervalSeconds = monitorIntervalSeconds;
+    this.resources = requireNonNull(resources, "resources must be supplied");
+  }
+
+  public int getMonitorIntervalSeconds() {
+    return monitorIntervalSeconds;
+  }
+
+  public Set<Resource> getResources() {
+    return Collections.unmodifiableSet(resources);
+  }
+
+  @Override
+  public int hashCode() {
+    return hash(monitorIntervalSeconds, resources);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    Manifest other = (Manifest) obj;
+    return monitorIntervalSeconds == other.monitorIntervalSeconds
+        && Objects.equals(resources, other.resources);
+  }
+
+  public String getChecksumAlgorithm() {
+    return SHA_512;
+  }
+
+  public String getChecksum() {
+    return checksum.get();
+  }
+
+  public String toJson() {
+    // GSON pretty print uses Unix line-endings, and may or may not have a trailing one, so
+    // ensure a trailing one exists, so it's included in checksum computations and in
+    // any files written from this (for better readability)
+    return GSON.toJson(this).stripTrailing() + "\n";
+  }
+
+  @Override
+  public String keyword() {
+    return "create-classloader-manifest";
+  }
+
+  @Override
+  public String description() {
+    return "Creates and prints a class loader context manifest for the CachingClassLoaderFactory";
+  }
+
+  @Override
+  public void execute(String[] args) throws Exception {
+    Opts opts = new Opts();
+    opts.parseArgs(Manifest.class.getName(), args);
+    URL[] urls = new URL[opts.files.size()];
+    int count = 0;
+    for (String f : opts.files) {
+      urls[count++] = new URL(f);
+    }
+    System.out.print(create(opts.monitorInterval, opts.algorithm, urls).toJson());
+  }
+}
