aba6a3414f3f36bd6b8195328785587bacd1b84a
Christopher Tubbs <ctubbsii@apache.org>
2026-02-18 16:49:37 -0500

    Fix QA checks

--- modules/caching-classloader/src/test/java/org/apache/accumulo/classloader/ccl/manifest/ManifestTest.java
+++ modules/caching-classloader/src/test/java/org/apache/accumulo/classloader/ccl/manifest/ManifestTest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.accumulo.classloader.ccl.manifest;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -8,7 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.io.ByteArrayInputStream;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
 
@@ -47,7 +65,7 @@ class ManifestTest {
 
   @Test
   void testCreate() throws Exception {
-    var manifest = Manifest.create(null, 27, "SHA-512", Paths.get("pom.xml").toUri().toURL());
+    var manifest = Manifest.create(null, 27, "SHA-512", Path.of("pom.xml").toUri().toURL());
     assertEquals(null, manifest.getComment());
     assertEquals(27, manifest.getMonitorIntervalSeconds());
     assertEquals("SHA-512", manifest.getChecksumAlgorithm());
